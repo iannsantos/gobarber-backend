98071813e3294ba40f8e88793e90dad367078b2e
Iann Santos <iannsantos8@gmail.com>
2019-10-21 23:29:37 -0300

    add integration with sentry for exceptions

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-named-as-default-member */
 import express from 'express';
 import { resolve } from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+// eslint-disable-next-line import/no-named-as-default
+import sentryConfig from './config/sentry';
+import 'express-async-errors';
 import routes from './routes';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     // express.static é para prover arquivos
     this.server.use(
@@ -21,6 +33,14 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://b9667553300b455994b5eab3a852b98b@sentry.io/1788764',
+};
